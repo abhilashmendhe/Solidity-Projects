7a9f40adea7abfb41e8fd9d63da30c722329e8c5
abhilashmendhe <abhilashmendhe@gmail.com>
2025-12-28 11:39:23 +0530

    Added about the task as comment

--- 01-Click Counter/ClickCounter.sol
+++ 01-Click Counter/ClickCounter.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30; // this is the solidity version
 
+/*
+    Tasks
+    -----
+    Let's build a simple counter! Imagine a digital clicker. You'll create a 'function' named `click()`. 
+    Each time someone calls this function, a number stored in the contract (a 'variable') will increase by one. 
+    You'll learn how to declare a variable to hold a number (an `uint`) and create functions to change it (increment/decrement). 
+    This is the very first step in making interactive smart contracts, showing how to store and modify data.
+*/
 
 contract ClickCounter {
 
