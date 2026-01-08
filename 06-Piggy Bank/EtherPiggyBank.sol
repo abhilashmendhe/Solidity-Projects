68c031c9e3eea5d19042b9715f2cbb0717dc2300
abhilashmendhe <abhilashmendhe@gmail.com>
2026-01-08 10:53:53 +0530

    Completed digital piggy bank project

--- 06-Piggy Bank/EtherPiggyBank.sol
+++ 06-Piggy Bank/EtherPiggyBank.sol
@@ -17,14 +17,58 @@ contract DigitalPiggyBank {
 
     BankManager bm;
 
-    address[] users;
-    mapping (address => bool) public registered_users;
-    mapping (address => uint256) users_balances;
+    modifier onlyBankManager() {
+        require(msg.sender==bm.bankManagerAddress, "Only bank manager can perform operations.");
+        _;
+    }
+
+    modifier onlyNormalMemeber() {
+        require(msg.sender!=bm.bankManagerAddress, "Only account holders can perform operations");
+        _;
+    }
+
+    struct User {
+        address userAddress;
+        uint256 amt;
+        bool registered;
+    }
+    User[] users;
+    mapping (address => User) public registered_users;
 
 
     constructor(string memory name) {
         bm = BankManager(name, msg.sender, 0);
     }
 
-    
+    function createAccount() public payable onlyNormalMemeber {
+
+        require(registered_users[msg.sender].userAddress==0x0000000000000000000000000000000000000000, "Account already exists.");
+        require(msg.value>=50000000000000, "Minimum wei atleast 50000000000000");
+        uint256 bankFee = 5000000000000;
+        User memory user = User(msg.sender, msg.value-bankFee, false);
+        registered_users[msg.sender] = user;
+        users.push(user);
+        bm.balance += bankFee;
+    }
+    function getMember(address _user) public view onlyBankManager returns(User memory) {
+        return registered_users[_user];
+    }
+    function getAllMembers() public view onlyBankManager returns (User[] memory) {
+        return users;
+    }
+    function approveAccount(address user) public onlyBankManager {
+        registered_users[user].registered = true;
+    }
+    function deposit() public payable onlyNormalMemeber {
+        require(registered_users[msg.sender].userAddress!=0x0000000000000000000000000000000000000000, "Account does not exists.");
+        uint256 bankFee = 1000000000000;
+        registered_users[msg.sender].amt += (msg.value-bankFee);
+        bm.balance += bankFee;
+    }
+
+    function withdraw() public payable onlyNormalMemeber {
+        require(registered_users[msg.sender].userAddress!=0x0000000000000000000000000000000000000000, "Account does not exists.");
+        registered_users[msg.sender].amt -= msg.value;
+    }
 }
+
