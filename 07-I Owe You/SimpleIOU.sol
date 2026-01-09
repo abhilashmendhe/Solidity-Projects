bb8a32c4700283400c87cbe36fecea87f64c0b50
abhilashmendhe <abhilashmendhe@gmail.com>
2026-01-09 12:37:17 +0530

    Half way done with IOU code

--- 07-I Owe You/SimpleIOU.sol
+++ 07-I Owe You/SimpleIOU.sol
@@ -22,11 +22,11 @@ contract SimpleIOU {
     Friend[] public friendGroup;
     mapping (address => Friend) public friendsInfo; 
 
-    mapping (address => mapping (address=>Friend)) public debts;
+    mapping (address => mapping (address=>uint256)) public debts;
 
 
     modifier onlyOwner() {
-        require(i_owner==msg.sender, "Only owner can perform operations.");
+        require(i_owner==msg.sender, "Only owner can perform the operations.");
         _;
     }
 
@@ -38,8 +38,32 @@ contract SimpleIOU {
         friendsInfo[msg.sender] = mySelf;
     }
 
+    function getAllFriends() public view returns (Friend[] memory) {
+        return friendGroup;
+    }
+    function addFriend(address _friendAddr) public onlyOwner {
+        require(_friendAddr != address(0), "Friend's address is invalid.");
+        require(!friendsInfo[_friendAddr].inGroup, "Friend is already in the group.");
+        Friend memory friend = Friend(_friendAddr, 0, true);
+        friendGroup.push(friend);
+        friendsInfo[_friendAddr] = friend;
+    }
 
+    function addDebtor(address _debtor, uint256 amt) public {
+        require(_debtor != address(0), "Friend's address is invalid.");
+        require(!friendsInfo[_debtor].inGroup, "Friend is already in the group.");
+        require(amt > 0, "Amount should be greater than 0");
+        debts[msg.sender][_debtor] += amt;
+    }
 
+    function resetDebt(address _debtor) public {
+        require(_debtor != address(0), "Friend's address is invalid.");
+        require(!friendsInfo[_debtor].inGroup, "Friend is already in the group.");
+        debts[msg.sender][_debtor] = 0;
+    }
 
-
+    function checkYourInfo() public view returns (Friend memory) {
+        require(!friendsInfo[msg.sender].inGroup, "You are not registered.");
+        return friendsInfo[msg.sender];
+    }
 }
