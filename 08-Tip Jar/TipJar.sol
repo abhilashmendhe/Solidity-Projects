64633a8db9a155e3281abd496ae1d0daf62cbb65
abhilashmendhe <abhilashmendhe@gmail.com>
2026-01-14 13:00:12 +0530

    Done with basic tipping ETH program. Now working on tipping USD or EUR

--- 08-Tip Jar/TipJar.sol
+++ 08-Tip Jar/TipJar.sol
@@ -7,7 +7,49 @@ pragma solidity ^0.8.3;
     Think of it like an advanced version of a 'Buy Me a Coffee' button — but smarter, more global, and Solidity-powered.
 */
 
+error NotOwner();
+
 contract TipJar {
 
+    address private immutable iOwner;
+
+    struct Tipper {
+        address tipperAddr;
+        uint256 donationAmt;
+    }
+
+    Tipper[] private tippers;
+    // mapping (address => uint256) private tippersDonationAmt;
+
+    constructor() {
+        iOwner = msg.sender;
+    }
+    function _onlyOwner() internal view {
+        if (msg.sender != iOwner) revert NotOwner();
+        
+    }
+    modifier onlyOnwer() {
+        _onlyOwner();
+        _;
+    }
+    function getOwner() public view onlyOnwer returns(address) {
+        return iOwner;
+    }
+
+    function viewTippers() public view onlyOnwer returns(Tipper[] memory) {
+        return tippers;
+    }
     
+    function tip() public payable {
+        require(msg.sender != iOwner, "Onwer can't fund itself.");
+        require(msg.value >= 300000000000000, "Minimum gas value should be 300000000000000 wei");
+        tippers.push(Tipper(msg.sender, msg.value));
+    }
+    
+    function withdraw() public onlyOnwer payable {
+        require(tippers.length > 0, "No tips are there to withdraw.");
+        delete tippers;
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Withdraw Failed.");
+    }
 }
