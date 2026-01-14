6923b2b4d9da302b7734d3193680ed58ff0ff1c7
abhilashmendhe <abhilashmendhe@gmail.com>
2026-01-14 10:06:04 +0530

    Started with a new solidity project called tip jar.

---
+++ 08-Tip Jar/TipJar.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+/*
+    Build a multi-currency digital tip jar! Users can send Ether directly or simulate tips in foreign currencies like USD or EUR. 
+    You'll learn how to manage currency conversion, handle Ether payments using `payable` and `msg.value`, and keep track of individual contributions. 
+    Think of it like an advanced version of a 'Buy Me a Coffee' button — but smarter, more global, and Solidity-powered.
+*/
+
+contract TipJar {
+
+    
+}
