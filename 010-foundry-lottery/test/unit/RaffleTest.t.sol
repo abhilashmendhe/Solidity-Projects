e6c7e48baafa0cbd97c926dc4edbdbe37ab89136
abhilashmendhe <abhilashmendhe@gmail.com>
2026-02-23 12:13:57 +0530

    wrote my first test script

---
+++ 010-foundry-lottery/test/unit/RaffleTest.t.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract RaffleTest is Test {
+
+    Raffle public raffle; 
+    HelperConfig public helperConfig;
+
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint32 callbackGasLimit;
+    uint256 subscriptionId;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+    function setUp() external {
+         // create an instance of the deployer script
+        console.log("RaffleTest / setUp : creating deployer instance");
+        DeployRaffle deployer = new DeployRaffle();
+
+        console.log("RaffleTest / setUp : running the deployer");
+
+        (raffle, helperConfig) = deployer.deployContract();
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+        vrfCoordinator = config.vrfCoordinator;
+        gasLane = config.gasLane;
+        callbackGasLimit = config.callbackGasLimit;
+        subscriptionId = config.subscriptionId;
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+}
