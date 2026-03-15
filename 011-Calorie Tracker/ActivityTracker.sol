ebb4ee8de54ba959793f6dbfbdda454f9aa40a15
abhilashmendhe <abhilashmendhe@gmail.com>
2026-03-15 12:07:23 +0530

    started with another project where we learn emiting events

---
+++ 011-Calorie Tracker/ActivityTracker.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+/*
+    Create a smart contract that logs user workouts and emits events when fitness goals are 
+    reached — like 10 workouts in a week or 500 total minutes. Users log each session (type, duration, calories), 
+    and the contract tracks progress. Events use *indexed* parameters to make it easy for frontends or 
+    off-chain tools to filter logs by user and milestone — just like a backend for a decentralized fitness 
+    tracker with achievement unlocks.
+*/
+
+struct UserSession {
+    string workoutType;
+    uint256 startTime;
+    uint256 endTime;
+    uint256 totalWorkoutTime;
+    uint256 recordTime;
+    uint256 caloriesBurnt;
+}
+
+enum Gender { Male, Female }
+
+struct User {
+    string name;
+    uint8 age;
+    uint8 height;
+    uint16 weight;
+    Gender gender;
+    uint256 sessionCount;
+}
+
+contract ActivityTracker {
+
+
+    
+    address[] public users;
+    mapping(address => User) public logUsers;
+    mapping(address => mapping(uint256 => UserSession)) public userSessions;
+    
+    modifier registerCheck(string memory name, uint8 age, uint8 height, uint16 weight) {
+        require(keccak256(bytes(name)) != keccak256(bytes("")), "Name should not be empty");
+        require(age < 100, "Age should not be more than 100");
+        require(height < 200, "Height should not exceed more than 200 cm");
+        require(weight < 600, "Weight should not exceed more than 600 kg");
+        _;
+    }
+
+    function registerUser(
+        string memory name, 
+        uint8 age, 
+        uint8 height, 
+        uint16 weight, 
+        Gender gender
+    ) public registerCheck(name, age, height, weight) {
+        
+        User memory tmpUser = User({
+            name: name, 
+            age: age, 
+            height: height, 
+            weight:  weight,
+            gender: gender,
+            sessionCount: 0
+        });
+
+        users.push(msg.sender);
+        logUsers[msg.sender] = tmpUser;
+    }   
+    
+}
