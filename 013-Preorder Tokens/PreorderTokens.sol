dca4f38332b2f67707697f62facfaf31776dc652
abhilashmendhe <abhilashmendhe@gmail.com>
2026-03-21 14:48:26 +0530

    added helper functions to my program

--- 013-Preorder Tokens/PreorderTokens.sol
+++ 013-Preorder Tokens/PreorderTokens.sol
@@ -10,14 +10,19 @@ import "./MyMenToken.sol";
 
 contract PreorderTokens is MyMenToken {
     
-    address projectOwner;
-    uint256 tokenPrice;
-    uint256 saleStartTime;
-    uint256 saleEndTime;
-    uint256 minPurchaseAmt;
-    uint256 maxPurchaseAmt;
+    address private projectOwner;
+    uint256 private tokenPrice;
+    uint256 private saleStartTime;
+    uint256 private saleEndTime;
+    uint256 private minPurchaseAmt;
+    uint256 private maxPurchaseAmt;
+    uint256 private totalValueRaised;
     bool public finalized = false;
     bool private initialTransferDone = false;
+    MyMenToken public myToken;
+    
+    event PurchasedTokens(address indexed buyer, uint256 tokenAmt, uint256 ethAmt);
+    event SaleEnd(uint256 totalValRaised, uint256 totalSoldTokens);
 
     constructor(
         uint256 _initialSupply,
@@ -40,4 +45,20 @@ contract PreorderTokens is MyMenToken {
 
         initialTransferDone = true;
     }
+
+    
+    function saleRemaingTime() public view returns (uint256) {
+        require(block.timestamp <= saleEndTime, "Sale ended");
+        return saleEndTime - block.timestamp;
+    }
+    function availibilityOfTokens() public view returns (uint256) {
+        return balanceOf(address(this));
+    }
+    function totalTokenValueRaised() public view returns (uint256) {
+        return totalValueRaised;
+    }
+    function owner() public view returns(address) {
+        return projectOwner;
+    }
+    
 }
