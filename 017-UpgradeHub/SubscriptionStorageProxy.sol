b881abaf9a7de0fd7c9b50f91d71b7162c5408e5
abhilashmendhe <abhilashmendhe@gmail.com>
2026-03-25 15:14:37 +0530

    completed proxy contract where users can make use of this contract to contact other contracts

--- 017-UpgradeHub/SubscriptionStorageProxy.sol
+++ 017-UpgradeHub/SubscriptionStorageProxy.sol
@@ -1,8 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.31;
 
-// This contract stores the data.
+// This contract stores the data and will help users to interact with the business logic from real contracts where the logic lives
 
-contract SubscriptionStorageProxy {
+import "./SubscriptionStorageLayout.sol";
 
+contract SubscriptionStorageProxy is SubscriptionStorageLayout {
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not the owner.");
+        _;
+    }
+
+    constructor(address _logicContract) {
+        owner = msg.sender;
+        logicContract = _logicContract;
+    }
+
+    function upgradeTo(address _newLogicContract) external onlyOwner {
+        logicContract = _newLogicContract;
+    }
+
+    // this function gets triggered everytime when users try to interact with a function
+    fallback() external payable { 
+        address implementation = logicContract;
+        require(implementation != address(0), "No logic contract found!");
+
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            
+            switch result
+                case 0 {revert(0, returndatasize())}
+                default {return(0, returndatasize())}
+        }
+    }
+
+    receive() external payable { 
+        
+    }
 }
