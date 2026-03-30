3e8ec8bccb223e10b0e539308f9586dc2ccc9faa
Abhilash <abhilashmendhe@gmail.com>
2026-03-30 21:10:03 +0530

    wrote custom stable coin erc-20 token

---
+++ foundry-defi-stablecoin/src/DecentralizedStableCoin.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Abhilash Mendhe
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stable: Pegged to USD
+ * 
+ * This is a contract governed by DSC engine. 
+ * Contract is implemeted purely in ERC20
+ */
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {
+        
+    }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount); // use burn function from parent file
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero(); 
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+
+}
