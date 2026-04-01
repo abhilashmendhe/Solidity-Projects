cbf5e988bd48ff7f73711a9807e007c7b77762f0
abhilashmendhe <abhilashmendhe@gmail.com>
2026-04-01 10:35:45 +0530

    updated GoldVault.sol, added description about the project

--- 020-FortKnox/GoldVault.sol
+++ 020-FortKnox/GoldVault.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.31;
 
+/*
+    Build a secure digital vault where users can deposit and withdraw tokenized gold (or any valuable asset), ensuring it's protected from 
+    reentrancy attacks. Imagine you're creating a decentralized version of Fort Knox — users lock up tokenized gold, and can later withdraw it. 
+    But just like a real vault, this contract must prevent attackers from repeatedly triggering the withdrawal logic before the balance updates. 
+    You'll implement the `nonReentrant` modifier to block reentry attempts, and follow Solidity security best practices to lock down your 
+    contract. This project shows how a seemingly simple withdrawal function can become a vulnerability — and how to defend it properly.
+*/
+
 contract GoldVault {
 
     error GoldVault__EntranceRestricted();
