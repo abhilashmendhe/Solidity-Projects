cb26a827eb54780c507d4f1afccb85428970af4a
Abhilash <abhilashmendhe@gmail.com>
2026-04-02 14:41:07 +0530

    added more tests

--- foundry-defi-stablecoin/test/unit/DSCEngineTest.t.sol
+++ foundry-defi-stablecoin/test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {ERC20Mock} from "../../test/mocks/ERC20Mock.sol";
+import {console} from "forge-std/console.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,7 +16,9 @@ contract DSCEngineTest is Test {
     HelperConfig config;
 
     address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
     address wEth;
+    address wBtc;
 
     uint256 amountCollateral = 10 ether;
     uint256 amountToMint = 100 ether;
@@ -27,12 +30,27 @@ contract DSCEngineTest is Test {
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dscEngine, config) = deployer.run();
-        (ethUsdPriceFeed,, wEth,,) = config.activeNetworkConfig();
+        (ethUsdPriceFeed, btcUsdPriceFeed, wEth, wBtc,) = config.activeNetworkConfig();
 
         ERC20Mock(wEth).mint(user, STARTING_ERC20_BALANCE);
     }
 
-    // ----------------  Price Tests  -----------------
+    // ----------------  Constructor Tests  ----------------
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function testRevertsIfTokenLengthMismatchPriceFeeds() public {
+        tokenAddresses.push(wEth);
+        // tokenAddresses.push(wBtc); // Make sure to comment this so the test can pass
+        priceFeedAddresses.push(ethUsdPriceFeed);
+        priceFeedAddresses.push(btcUsdPriceFeed);
+
+        vm.expectRevert(DSCEngine.DSCEngine__TokenAddressAndPriceFeedAddressMustBeSameLength.selector);
+        new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        // new DSCEngine
+    }
+
+    // ----------------  Price Tests  ----------------
     function testGetUsdValue() public {
         uint256 ethAmount = 15e18;
         // 15e18 * 2000/ETH = 30,000e18;
@@ -40,6 +58,15 @@ contract DSCEngineTest is Test {
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dscEngine.getUsdValue(wEth, ethAmount);
         assert(expectedUsd == actualUsd);
+    }
+
+    function testGetTokenAmountFromUsd() public {
+        uint256 usdAmount = 100 ether;
+        // $2000 / ETH, $100 => $100 / $ 2000 ETH = 0.05 ETH = 50000000000000000 wei
+        uint256 expectedWeth = 0.05 ether;
+        uint256 actualWeth = dscEngine.getTokenAmountFromUsd(wEth, usdAmount);
+        assert(expectedWeth == actualWeth);
+        // console.log(actualWeth, expectedWeth);
     }
 
     // ----------------  depositCollateral Tests  -----------------
@@ -50,5 +77,32 @@ contract DSCEngineTest is Test {
         vm.expectRevert(DSCEngine.DSCEngine__NeedMoreThanZero.selector);
         dscEngine.depositCollateral(wEth, 0);
         vm.stopPrank();
+    }
+
+    function testRevertWithUnprovedCollateral() public {
+        ERC20Mock ranToken = new ERC20Mock("RAN", "RANAA", user, amountCollateral);
+        vm.startPrank(user);
+        vm.expectRevert(DSCEngine.DSCEngine__NotAllowedToken.selector);
+        dscEngine.depositCollateral(address(ranToken), amountCollateral);
+        vm.stopPrank();
+    }
+
+    modifier depositedCollateral() {
+        vm.startPrank(user);
+        ERC20Mock(wEth).approve(address(dscEngine), amountCollateral);
+        dscEngine.depositCollateral(wEth, amountCollateral);
+        vm.stopPrank();
+        _;
+    }
+
+    function testCanDepositCollateralAndGetAccountInfo() public depositedCollateral {
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dscEngine.getAccountInformation(user);
+
+        // 10 eth * 2000 = 20,000
+        uint256 expectedTotalDscMinted = 0;
+        uint256 expectedDepositAmount = dscEngine.getTokenAmountFromUsd(wEth, collateralValueInUsd);
+
+        assert(totalDscMinted == expectedTotalDscMinted);
+        assert(amountCollateral == expectedDepositAmount);
     }
 }
