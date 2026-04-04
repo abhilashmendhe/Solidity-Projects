4742f5931e2c84d783337804419220c419353307
Abhilash <abhilashmendhe@gmail.com>
2026-04-04 10:24:02 +0530

    added balanceOf virtual function

--- ccip-rebase-token/src/IRebaseToken.sol
+++ ccip-rebase-token/src/IRebaseToken.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.31;
 abstract contract IRebaseToken {
     function mint(address _to, uint256 _amount) external virtual;
     function burn(address _from, uint256 _amount) external virtual;
+    function balanceOf(address _account) external view virtual returns (uint256);
 }
