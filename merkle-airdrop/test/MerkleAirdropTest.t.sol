72c8149d0f6ab5d518d88113b2ffdd47fa2f16ca
Abhilash <abhilashmendhe@gmail.com>
2026-04-06 15:23:34 +0530

    added a test where a verified user can make claim

--- merkle-airdrop/test/MerkleAirdropTest.t.sol
+++ merkle-airdrop/test/MerkleAirdropTest.t.sol
@@ -18,11 +18,13 @@ contract MerkleAirdropTest is Test, ZkSyncChainChecker {
     bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public PROOF = [proofOne, proofTwo];
+    address gasPayer;
     address user;
     uint256 userPrivKey;
 
     function setUp() public {
         (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
         if (!isZkSyncChain()) {
             // deploy with scripts
             DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
@@ -38,8 +40,15 @@ contract MerkleAirdropTest is Test, ZkSyncChainChecker {
     function testUserCanClaim() public {
         // console.log("user address:", user);
         uint256 startingBalance = token.balanceOf(user);
-        vm.prank(user);
-        airDrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        bytes32 digest = airDrop.getMessage(user, AMOUNT_TO_CLAIM);
+
+        // we want user to sign a message
+        // vm.prank(user);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        // gasPayer calls claim using the signed message
+        vm.prank(gasPayer);
+        airDrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(user);
         console.log("Ending balance: ", endingBalance);
