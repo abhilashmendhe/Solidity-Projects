8e356778e54de27944d5537b9f5f13919807d202
Abhilash <abhilashmendhe@gmail.com>
2026-04-08 11:26:27 +0530

    modified file

--- foundry-account-abstraction/script/SendPackedUserOp.s.sol
+++ foundry-account-abstraction/script/SendPackedUserOp.s.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.31;
 import {Script} from "forge-std/Script.sol";
 import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
 
-contract SendPackedUserOp is Script {}
+contract SendPackedUserOp is Script {
+
+    function run() public {
+
+    }
+
+    function deployMinimalAccount() public {}
+}
